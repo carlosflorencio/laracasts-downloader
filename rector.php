d65d613bc4b93373c299fec3628533ba5bcc9590
Eugene Owak <teugeneowak@gmail.com>
2024-12-10 09:26:27 +0300

    Update to latest
    - update packages to latest
    - require php8.3
    - add php rector and configure it

---
+++ rector.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/App',
+    ])
+    ->withPhpSets(php83:true)
+    ->withPreparedSets(
+        deadCode: true,  // Enable dead code removal
+        codeQuality: true,  // Enable code quality improvements
+        naming: false  // Disable naming convention refactoring
+    )
+    ->withTypeCoverageLevel(0);
